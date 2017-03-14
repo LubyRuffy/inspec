6c3cfc863ff24cf502fd6fadb82da2ebc0354818
Dominik Richter <dominik.richter@gmail.com>
2017-03-14 22:23:50 +0100

    restrict all properties in json schema
    We should not have any additional fields included that we didn't define. This will ensure a strict style across InSpec's default JSON outputs.
    
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- lib/utils/schema/schema.rb
+++ lib/utils/schema/schema.rb
@@ -7,33 +7,44 @@ def generate(name, schema)
 end
 
 statistics = {
+  'type' => 'object',
+  'additionalProperties' => false,
   'properties' => {
     'duration' => { 'type' => 'number' },
   },
 }
 
+# Tags are open right, with simple key-value associations and not restrictions
 tags = { 'type' => 'object' }
 
 result = {
   'type' => 'object',
+  'additionalProperties' => false,
   'properties' => {
     'status' => { 'type' => 'string' },
     'code_desc' => { 'type' => 'string' },
     'run_time' => { 'type' => 'number' },
     'start_time' => { 'type' => 'string' },
+    'skip_message' => { 'type' => 'string', 'optional' => true },
+    'resource' => { 'type' => 'string', 'optional' => true },
   },
 }
 
 ref = {
   'type' => 'object',
+  'additionalProperties' => false,
   'properties' => {
-    'url' => { 'type' => 'string', 'optional' => true },
     'ref' => { 'type' => 'string' },
+    # TODO: One of these needs to be deprecated
+    'uri' => { 'type' => 'string', 'optional' => true },
+    'url' => { 'type' => 'string', 'optional' => true },
   },
 }
 refs = { 'type' => 'array', 'items' => ref }
 
 control = {
+  'type' => 'object',
+  'additionalProperties' => false,
   'properties' => {
     'id' => { 'type' => 'string' },
     'title' => { 'type' => %w{string null} },
@@ -55,21 +66,36 @@ control = {
 
 supports = {
   'type' => 'object',
+  'additionalProperties' => false,
   'properties' => {
     'os-family' => { 'type' => 'string', 'optional' => true },
   },
 }
 
+control_group = {
+  'type' => 'object',
+  'additionalProperties' => false,
+  'properties' => {
+    'id' => { 'type' => 'string' },
+    'title' => { 'type' => 'string', 'optional' => true },
+    'controls' => { 'type' => 'array', 'items' => { 'type' => 'string' }},
+  },
+}
+
 profile = {
   'type' => 'object',
+  'additionalProperties' => false,
   'properties' => {
+    'name' => { 'type' => 'string' },
+    'version' => { 'type' => 'string', 'optional' => true },
+
     'title' => { 'type' => 'string', 'optional' => true },
     'maintainer' => { 'type' => 'string', 'optional' => true },
     'copyright' => { 'type' => 'string', 'optional' => true },
     'copyright_email' => { 'type' => 'string', 'optional' => true },
     'license' => { 'type' => 'string', 'optional' => true },
     'summary' => { 'type' => 'string', 'optional' => true },
-    'version' => { 'type' => 'string', 'optional' => true },
+
     'supports' => {
       'type' => 'array',
       'items' => supports,
@@ -79,11 +105,20 @@ profile = {
       'type' => 'array',
       'items' => control,
     },
+    'groups' => {
+      'type' => 'array',
+      'items' => control_group,
+    },
+    'attributes' => {
+      'type' => 'array',
+      # TODO: more detailed specification needed
+    },
   },
 }
 
 exec_full = {
   'type' => 'object',
+  'additionalProperties' => false,
   'properties' => {
     'profiles' => {
       'type' => 'array',
@@ -96,13 +131,13 @@ exec_full = {
     'controls' => 'array',
     'other_checks' => 'array',
   },
-  'additionalProperties' => false,
 }
 
 generate('inspec.exec.full.json', exec_full)
 
 min_control = {
   'type' => 'object',
+  'additionalProperties' => false,
   'properties' => {
     'id' => { 'type' => 'string' },
     'profile_id' => { 'type' => %w{string null} },
@@ -115,6 +150,7 @@ min_control = {
 
 exec_min = {
   'type' => 'object',
+  'additionalProperties' => false,
   'properties' => {
     'statistics' => statistics,
     'version' => { 'type' => 'string' },
