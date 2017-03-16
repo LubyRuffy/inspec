b981b13fb955022d48bb60b0d0b99814b265f913
Dominik Richter <dominik.richter@gmail.com>
2017-03-16 16:55:58 +0100

    lint and encapsulate
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- lib/utils/schema/schema.rb
+++ lib/utils/schema/schema.rb
@@ -1,164 +1,166 @@
-#!/usr/bin/env ruby
+# encoding: utf-8
+# usage: ruby schema.rb
 require 'json'
 
-def generate(name, schema)
-  File.write(name, JSON.dump(schema))
-  puts "Schema created in #{name}"
-end
+module InSpecSchema # rubocop:disable Metrics/ModuleLength
+  def self.generate(name, schema)
+    File.write(name, JSON.dump(schema))
+    puts "Schema created in #{name}"
+  end
 
-statistics = {
-  'type' => 'object',
-  'additionalProperties' => false,
-  'properties' => {
-    'duration' => { 'type' => 'number' },
-  },
-}
+  Statistics = {
+    'type' => 'object',
+    'additionalProperties' => false,
+    'properties' => {
+      'duration' => { 'type' => 'number' },
+    },
+  }.freeze
 
-# Tags are open right, with simple key-value associations and not restrictions
-tags = { 'type' => 'object' }
+  # Tags are open right, with simple key-value associations and not restrictions
+  Tags = { 'type' => 'object' }.freeze
 
-result = {
-  'type' => 'object',
-  'additionalProperties' => false,
-  'properties' => {
-    'status' => { 'type' => 'string' },
-    'code_desc' => { 'type' => 'string' },
-    'run_time' => { 'type' => 'number' },
-    'start_time' => { 'type' => 'string' },
-    'skip_message' => { 'type' => 'string', 'optional' => true },
-    'resource' => { 'type' => 'string', 'optional' => true },
-  },
-}
+  Result = {
+    'type' => 'object',
+    'additionalProperties' => false,
+    'properties' => {
+      'status' => { 'type' => 'string' },
+      'code_desc' => { 'type' => 'string' },
+      'run_time' => { 'type' => 'number' },
+      'start_time' => { 'type' => 'string' },
+      'skip_message' => { 'type' => 'string', 'optional' => true },
+      'resource' => { 'type' => 'string', 'optional' => true },
+    },
+  }.freeze
 
-ref = {
-  'type' => 'object',
-  'additionalProperties' => false,
-  'properties' => {
-    'ref' => { 'type' => 'string' },
-    # TODO: One of these needs to be deprecated
-    'uri' => { 'type' => 'string', 'optional' => true },
-    'url' => { 'type' => 'string', 'optional' => true },
-  },
-}
-refs = { 'type' => 'array', 'items' => ref }
+  Ref = {
+    'type' => 'object',
+    'additionalProperties' => false,
+    'properties' => {
+      'ref' => { 'type' => 'string' },
+      # TODO: One of these needs to be deprecated
+      'uri' => { 'type' => 'string', 'optional' => true },
+      'url' => { 'type' => 'string', 'optional' => true },
+    },
+  }.freeze
+  Refs = { 'type' => 'array', 'items' => Ref }.freeze
 
-control = {
-  'type' => 'object',
-  'additionalProperties' => false,
-  'properties' => {
-    'id' => { 'type' => 'string' },
-    'title' => { 'type' => %w{string null} },
-    'desc' => { 'type' => %w{string null} },
-    'impact' => { 'type' => 'number' },
-    'refs' => refs,
-    'tags' => tags,
-    'code' => { 'type' => 'string' },
-    'source_location' => {
-      'type' => 'object',
-      'properties' => {
-        'ref' => { 'type' => 'string' },
-        'line' => { 'type' => 'number' },
+  Control = {
+    'type' => 'object',
+    'additionalProperties' => false,
+    'properties' => {
+      'id' => { 'type' => 'string' },
+      'title' => { 'type' => %w{string null} },
+      'desc' => { 'type' => %w{string null} },
+      'impact' => { 'type' => 'number' },
+      'refs' => Refs,
+      'tags' => Tags,
+      'code' => { 'type' => 'string' },
+      'source_location' => {
+        'type' => 'object',
+        'properties' => {
+          'ref' => { 'type' => 'string' },
+          'line' => { 'type' => 'number' },
+        },
+      },
+      'results' => { 'type' => 'array', 'items' => Result },
+    },
+  }.freeze
+
+  Supports = {
+    'type' => 'object',
+    'additionalProperties' => false,
+    'properties' => {
+      'os-family' => { 'type' => 'string', 'optional' => true },
+    },
+  }.freeze
+
+  Control_group = {
+    'type' => 'object',
+    'additionalProperties' => false,
+    'properties' => {
+      'id' => { 'type' => 'string' },
+      'title' => { 'type' => 'string', 'optional' => true },
+      'controls' => { 'type' => 'array', 'items' => { 'type' => 'string' } },
+    },
+  }.freeze
+
+  Profile = {
+    'type' => 'object',
+    'additionalProperties' => false,
+    'properties' => {
+      'name' => { 'type' => 'string' },
+      'version' => { 'type' => 'string', 'optional' => true },
+
+      'title' => { 'type' => 'string', 'optional' => true },
+      'maintainer' => { 'type' => 'string', 'optional' => true },
+      'copyright' => { 'type' => 'string', 'optional' => true },
+      'copyright_email' => { 'type' => 'string', 'optional' => true },
+      'license' => { 'type' => 'string', 'optional' => true },
+      'summary' => { 'type' => 'string', 'optional' => true },
+
+      'supports' => {
+        'type' => 'array',
+        'items' => Supports,
+        'optional' => true,
+      },
+      'controls' => {
+        'type' => 'array',
+        'items' => Control,
+      },
+      'groups' => {
+        'type' => 'array',
+        'items' => Control_group,
+      },
+      'attributes' => {
+        'type' => 'array',
+        # TODO: more detailed specification needed
       },
     },
-    'results' => { 'type' => 'array', 'items' => result },
-  },
-}
+  }.freeze
 
-supports = {
-  'type' => 'object',
-  'additionalProperties' => false,
-  'properties' => {
-    'os-family' => { 'type' => 'string', 'optional' => true },
-  },
-}
+  Exec_full = {
+    'type' => 'object',
+    'additionalProperties' => false,
+    'properties' => {
+      'profiles' => {
+        'type' => 'array',
+        'items' => Profile,
+      },
+      'statistics' => Statistics,
+      'version' => { 'type' => 'string' },
 
-control_group = {
-  'type' => 'object',
-  'additionalProperties' => false,
-  'properties' => {
-    'id' => { 'type' => 'string' },
-    'title' => { 'type' => 'string', 'optional' => true },
-    'controls' => { 'type' => 'array', 'items' => { 'type' => 'string' }},
-  },
-}
-
-profile = {
-  'type' => 'object',
-  'additionalProperties' => false,
-  'properties' => {
-    'name' => { 'type' => 'string' },
-    'version' => { 'type' => 'string', 'optional' => true },
-
-    'title' => { 'type' => 'string', 'optional' => true },
-    'maintainer' => { 'type' => 'string', 'optional' => true },
-    'copyright' => { 'type' => 'string', 'optional' => true },
-    'copyright_email' => { 'type' => 'string', 'optional' => true },
-    'license' => { 'type' => 'string', 'optional' => true },
-    'summary' => { 'type' => 'string', 'optional' => true },
-
-    'supports' => {
-      'type' => 'array',
-      'items' => supports,
-      'optional' => true,
+      # DEPRECATED PROPERTIES!! These will be removed with the next major version bump
+      'controls' => 'array',
+      'other_checks' => 'array',
     },
-    'controls' => {
-      'type' => 'array',
-      'items' => control,
+  }.freeze
+
+  Min_control = {
+    'type' => 'object',
+    'additionalProperties' => false,
+    'properties' => {
+      'id' => { 'type' => 'string' },
+      'profile_id' => { 'type' => %w{string null} },
+      'status' => { 'type' => 'string' },
+      'code_desc' => { 'type' => 'string' },
+      'skip_message' => { 'type' => 'string', 'optional' => true },
+      'resource' => { 'type' => 'string', 'optional' => true },
     },
-    'groups' => {
-      'type' => 'array',
-      'items' => control_group,
+  }.freeze
+
+  Exec_min = {
+    'type' => 'object',
+    'additionalProperties' => false,
+    'properties' => {
+      'statistics' => Statistics,
+      'version' => { 'type' => 'string' },
+      'controls' => {
+        'type' => 'array',
+        'items' => Min_control,
+      },
     },
-    'attributes' => {
-      'type' => 'array',
-      # TODO: more detailed specification needed
-    },
-  },
-}
+  }.freeze
+end
 
-exec_full = {
-  'type' => 'object',
-  'additionalProperties' => false,
-  'properties' => {
-    'profiles' => {
-      'type' => 'array',
-      'items' => profile,
-    },
-    'statistics' => statistics,
-    'version' => { 'type' => 'string' },
-
-    # DEPRECATED PROPERTIES!! These will be removed with the next major version bump
-    'controls' => 'array',
-    'other_checks' => 'array',
-  },
-}
-
-generate('inspec.exec.full.json', exec_full)
-
-min_control = {
-  'type' => 'object',
-  'additionalProperties' => false,
-  'properties' => {
-    'id' => { 'type' => 'string' },
-    'profile_id' => { 'type' => %w{string null} },
-    'status' => { 'type' => 'string' },
-    'code_desc' => { 'type' => 'string' },
-    'skip_message' => { 'type' => 'string', 'optional' => true },
-    'resource' => { 'type' => 'string', 'optional' => true },
-  },
-}
-
-exec_min = {
-  'type' => 'object',
-  'additionalProperties' => false,
-  'properties' => {
-    'statistics' => statistics,
-    'version' => { 'type' => 'string' },
-    'controls' => {
-      'type' => 'array',
-      'items' => min_control,
-    },
-  },
-}
-
-generate('inspec.exec.min.json', exec_min)
+InSpecSchema.generate('inspec.exec.full.json', InSpecSchema::Exec_full)
+InSpecSchema.generate('inspec.exec.min.json', InSpecSchema::Exec_min)
